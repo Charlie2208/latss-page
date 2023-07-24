7cdde0160ead07b5091ae07a2f6c347ba9f08b2d
Carlos Araya <c.araya@uroff.net>
2023-07-24 14:21:44 -0400

    add google verification to head

--- app/head.jsx
+++ app/head.jsx
@@ -4,6 +4,7 @@ export default function Head() {
         <title>Los Animales Tambien Se Sucidan</title>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
         <link rel='icon' type="image/png" href='./logolatss.png' />
+        <meta name="google-site-verification" content="TRSTQPSxOEiZkfM5ZKh8VQxG72Sh-kLY_fu8trLSEhY" />
       </>
     )
   }
