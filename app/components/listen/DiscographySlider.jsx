23260c0dd0c9e8d0c53307ced37dcc75530555e1
Carlos Araya <c.araya@uroff.net>
2023-07-24 23:27:38 -0400

    fix some erros

--- app/components/listen/DiscographySlider.jsx
+++ app/components/listen/DiscographySlider.jsx
@@ -1,4 +1,7 @@
 'use client'
+// DiscographySlider.js
+
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import 'swiper/css';
@@ -6,38 +9,40 @@ import './DiscographySlider.css';
 
 const DiscographySlider = ({ albums }) => {
   return (
-    <Swiper
-      effect={'coverflow'}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={'auto'}
-      coverflowEffect={{
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      pagination={true}
-      modules={[EffectCoverflow, Pagination]}
-      className="albumes-container-sw"
-    >
-      {albums.map((album, index) => (
-        <SwiperSlide className='albumes-sl-sw' key={`albumes-${index}`}>
-          <div className="album-card">
-            <img className='images-albumes' src={album.cover} alt={album.title} />
-            <a
-              href={album.listenLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="listen-button"
-            >
-              Escucha
-            </a>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="albumes-container-sw"
+      >
+        {albums.map((album, index) => (
+          <SwiperSlide className='albumes-sl-sw' key={`albumes-${index}`}>
+            <div className="album-card">
+              <img className='images-albumes' src={album.cover} alt={album.title} />
+              <a
+                href={album.listenLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="listen-button"
+              >
+                Escucha
+              </a>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
