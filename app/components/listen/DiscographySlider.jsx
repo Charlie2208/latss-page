6da2524939829a81f9aafc8830907014dccf0bcc
Carlos Araya <c.araya@uroff.net>
2023-07-24 23:41:04 -0400

    add navigation to listen

--- app/components/listen/DiscographySlider.jsx
+++ app/components/listen/DiscographySlider.jsx
@@ -1,46 +1,56 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import './DiscographySlider.css';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 const DiscographySlider = ({ albums }) => {
   return (
-    <Swiper
-      effect={'coverflow'}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={'auto'}
-      coverflowEffect={{
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      pagination={true}
-      modules={[EffectCoverflow, Pagination]}
-      className="albumes-container-sw"
-    >
-      {albums.map((album, index) => (
-        <SwiperSlide className='albumes-sl-sw' key={`albumes-${index}`}>
-          <div className="album-card">
-            <img className='images-albumes' src={album.cover} alt={album.title} />
-            <a
-              href={album.listenLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="listen-button"
-            >
-              Escucha
-            </a>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="swiper-container">
+      {/* Botones de navegación */}
+      <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
+
+      {/* Swiper */}
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="albumes-container-sw"
+      >
+        {albums.map((album, index) => (
+          <SwiperSlide className="albumes-sl-sw" key={`albumes-${index}`}>
+            <div className="album-card">
+              <img className="images-albumes" src={album.cover} alt={album.title} />
+              <a
+                href={album.listenLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="listen-button"
+              >
+                Escucha
+              </a>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
 export default DiscographySlider;
+
 
 
