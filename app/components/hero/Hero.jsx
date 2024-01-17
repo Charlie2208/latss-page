4ec41aeacfbb6b9eb65407fa29497fbd1a183369
Carlos Araya <c.araya@uroff.net>
2024-01-17 19:33:48 -0300

    change images carousel

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -3,12 +3,12 @@ import SwiperComponent from "../swiper/SwiperComponent"
 
 const Hero  = () => {
     const images = [
-        { name: 'latss-retrato-margan', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5542.jpg' },
-        { name: 'latss-retrato-charlie', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5571.jpg' },
-        { name: 'latss-retrato-fernando', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5596.jpg' },
+        { name: 'latss-retrato-margan', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/retrato_margan.jpg' },
+        { name: 'latss-retrato-charlie', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/retrato_charlie.jpg' },
+        { name: 'latss-retrato-fernando', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/retrato_taka.jpg' },
         { name: 'latss-retrato-nacho', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5554.jpg' },
-        { name: 'latss-retrato-pau', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5633.jpg' },
-        { name: 'latss-retrato-chito', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/27.12_LATSS5531.jpg' },
+        { name: 'latss-retrato-pau', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/retrato_pau.jpg' },
+        { name: 'latss-retrato-chito', link: 'https://imagenes-portafolio.s3.amazonaws.com/fotos-latss/retrato_chito.jpg' },
       ];
     
     return(
